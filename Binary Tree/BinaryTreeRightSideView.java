732b1ad1a10929b5869c9d447d35bff9207ccfa6
Zhian Mai <zmai@ucsd.edu>
2021-09-30 00:37:00 -0700

    Add new codes

---
+++ Binary Tree/BinaryTreeRightSideView.java
@@ -0,0 +1,28 @@
+class BinaryTreeRightSideView {
+  public List<Integer> rightSideView(TreeNode root) {
+    List<Integer> result = new ArrayList<>();
+    if (root == null) {
+      return result;
+    }
+
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+
+    while (!queue.isEmpty()) {
+      int size = queue.size();
+      result.add(queue.peek().val);
+
+      while (size-- != 0) {
+        TreeNode curr = queue.poll();
+
+        if (curr.right != null) {
+          queue.offer(curr.right);
+        }
+        if (curr.left != null) {
+          queue.offer(curr.left);
+        }
+      }
+    }
+    return result;
+  }
+}
