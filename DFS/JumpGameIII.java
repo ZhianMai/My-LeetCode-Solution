a471299d625135fa0b86a3692dd2d54647f32ac9
Zhian Mai <zmai@ucsd.edu>
2021-09-29 22:20:20 -0700

    Add new codes

---
+++ DFS/JumpGameIII.java
@@ -0,0 +1,15 @@
+class JumpGameIII {
+    public boolean canReach(int[] arr, int index) {
+        if (index < 0 || index >= arr.length || arr[index] < 0) {
+            return false;
+        }
+        
+        arr[index] *= -1;
+        
+        if (arr[index] == 0) {
+            return true;
+        }
+        
+        return canReach(arr, index + arr[index]) || canReach(arr, index - arr[index]);
+    }
+}
