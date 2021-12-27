7b45219d53ecb074fa1c77a168ee4a7641963070
Zhian Mai <zmai@ucsd.edu>
2021-12-26 16:09:11 -0800

    binary tree camera

---
+++ Binary Tree/BinaryTreeCamera.java
@@ -0,0 +1,36 @@
+/**
+ * Problem #968
+ */
+class BinaryTreeCamera {
+  public int minCameraCover(TreeNode root) {
+    int[] sum = new int[]{0};
+    
+    if (check(root, sum) == -1) {
+      sum[0]++;
+    }
+    return sum[0];
+  }
+  
+  // -1: child needs cam covered, I need a cam!
+  // 0: child got covered, and I'm not covered.
+  // 1: child got cam, I'm good!
+  private int check(TreeNode root, int[] sum) {
+    if (root == null) {
+      return 0; // Parent take care of yourself!
+    }
+    int left = check(root.left, sum);
+    int right = check(root.right, sum);
+    
+    if (left == -1 || right == -1) { // Either child needs my cover
+      sum[0]++;
+      return 1; // My child need cam, I got one, and cover my parent.
+    }
+    
+    if (left == 1 || right == 1) { // One child covers me, and the other one got covered or cover me again.
+      return 0; // My child got cam, parent cover yourself.
+    }
+    
+    // None of my child needs cam and none of my child covers me! They must be 0!
+    return -1; // My child got coverd, and ask parent to cover me!
+  }
+}
