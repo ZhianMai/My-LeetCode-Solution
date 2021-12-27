ea1b267db4940f8c4ed2ef809e052d1d0b385f46
Zhian Mai <zmai@ucsd.edu>
2021-12-27 13:57:04 -0800

    k pairs with smallest sum

---
+++ BFS/KPairsWithSmallestSum.java
@@ -0,0 +1,53 @@
+/**
+ * Problem #373
+ */
+class KPairsWithSmallestSum {
+  class Pair {
+    long sum;
+    int i;
+    int j;
+    
+    public Pair(long sum, int i, int j) {
+      this.sum = sum;
+      this.i = i;
+      this.j = j;
+    }
+  }
+  
+  public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+    List<List<Integer>> result = new LinkedList<>();
+    int rows = Math.min(nums1.length, k);
+    int cols = Math.min(nums2.length, k);
+    boolean[][] visited = new boolean[rows][cols];
+    
+    // int[3]: [sum, nums1_idx, nums2_idx]
+    PriorityQueue<Pair> minHeap = new PriorityQueue<>(new Comparator<Pair>(){
+        @Override
+        public int compare(Pair a, Pair b) {
+          return Long.compare(a.sum, b.sum);
+        }
+    });
+    minHeap.offer(new Pair(nums1[0] + nums2[0], 0, 0));
+    visited[0][0] = true;
+    
+    while(k-- != 0 && !minHeap.isEmpty()) {
+      Pair pair = minHeap.poll();
+      List<Integer> curr = new LinkedList<>();
+      curr.add(nums1[pair.i]);
+      curr.add(nums2[pair.j]);
+      result.add(curr);
+      
+      if (pair.i + 1 < rows && !visited[pair.i + 1][pair.j]) {
+        minHeap.offer(new Pair(nums1[pair.i + 1] + nums2[pair.j], pair.i + 1, pair.j));
+        visited[pair.i + 1][pair.j] = true;
+      }
+      
+      if (pair.j + 1 < cols && !visited[pair.i][pair.j + 1]) {
+        minHeap.offer(new Pair(nums1[pair.i] + nums2[pair.j + 1], pair.i, pair.j + 1));
+        visited[pair.i][pair.j + 1] = true;
+      }
+    }
+    
+    return result;
+  }
+}
