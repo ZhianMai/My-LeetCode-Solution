99f0c0e9080f6f98825072a1467390c630a141f6
Zhian Mai <zmai@ucsd.edu>
2021-12-30 20:53:12 -0800

    Min distance between two nodes

--- Binary Tree/MinDistanceBetweenTwoNodes.java
+++ Binary Tree/MinDistanceBetweenTwoNodes.java
@@ -16,8 +16,10 @@ class MinDistanceBetweenTwoNodes {
   int findDist(Node root, int a, int b) {
     // Your code here
     int[] dist = new int[]{-1};
+    // Suppose one node is not the ancestor of another node
     findDistance1(root, a, b, dist);
     
+    // One node is the ancestor of another node
     if (dist[0] == -1) {
       findDistance2(root, a, b, dist, -1);
     }
@@ -25,6 +27,10 @@ class MinDistanceBetweenTwoNodes {
     return dist[0];
   }
     
+  /**
+   * Suppose two nodes are not the in the same path. If they're in the same
+   * path, then dist[] would not update.
+   */
   private int findDistance1(Node root, int a, int b, int[] dist) {
     if (root == null) {
       return -1;
@@ -38,11 +44,13 @@ class MinDistanceBetweenTwoNodes {
     int rightDist = findDistance1(root.right, a, b, dist);
     
     if (leftDist != -1 && rightDist != -1) {
+      // Update distance only on this node!
       dist[0] = leftDist + rightDist;
       return -1;
     } else if (leftDist == -1 && rightDist == -1) {
       return -1;
     } else {
+      // The current node is between (a || b) and their LCA
       return Math.max(leftDist, rightDist) + 1;
     }
   }
@@ -53,15 +61,16 @@ class MinDistanceBetweenTwoNodes {
     }
     
     if (root.data == a || root.data == b) {
-      if (height != -1) {
+      if (height != -1) { // One node is found and it's my ancestor
         dist[0] = height;
         return;
-      } else {
+      } else { // I'm the ancestor of another node
         findDistance2(root.left, a, b, dist, 1);
         findDistance2(root.right, a, b, dist, 1);
       }
     }
     
+    // I'm in between node a and node b
     if (height != -1) {
       height++;
     }
